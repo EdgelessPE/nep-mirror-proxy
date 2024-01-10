73c187ea13a3ce4342972caa60346889ad2f3d71
Cno <dongheming@bytedance.com>
2024-01-10 11:44:46 +0800

    feat: add terser

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,9 +1,11 @@
 import typescript from "@rollup/plugin-typescript";
+import terser from '@rollup/plugin-terser';
+
 export default {
   input: "src/index.ts",
   output: {
     file: "dist/bundle.cjs",
     format: "commonjs",
   },
-  plugins: [typescript()],
+  plugins: [typescript(),terser()],
 };
